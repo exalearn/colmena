4f89fa60bbad2308e425474b87f2b6f49596b25b
Yadu Nand B <yadudoc1729@gmail.com>
2019-11-14 15:55:38 -0600

    adding setup script

---
+++ setup.py
@@ -0,0 +1,42 @@
+import os
+from setuptools import setup, find_packages
+
+version_ns = {}
+with open(os.path.join("pipeline_prototype", "version.py")) as f:
+    exec(f.read(), version_ns)
+version = version_ns['VERSION']
+
+with open('requirements.txt') as f:
+    install_requires = f.readlines()
+
+setup(
+    name='pipeline_prototype',
+    version=version,
+    packages=find_packages(),
+    description='pipeline_prototype: Intelligent Steerable Pipelines on HPC',
+    install_requires=install_requires,
+    python_requires="==3.6.*",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering"
+    ],
+    keywords=[
+        "funcX",
+        "pipeline",
+        "HPC",
+    ],
+    entry_points={'console_scripts':
+                  ['pipeline-main=pipeline_prototype.main:cli_run',
+                   'pipeline-pump=pipeline_prototype.pump:cli_run',
+                  ]
+    },
+    author="Globus Labs",
+    author_email='labs@globus.org',
+    license="Apache License, Version 2.0",
+    url="https://github.com/funcx-faas/funcx"
+)
