36687981fdbe4a895a294ea4d32ac88ac270a10a
Yadu Nand B <yadudoc1729@gmail.com>
2019-12-04 11:08:17 -0600

    Removing older file paths that have been restructured into module

--- method_server/__init__.py
+++
@@ -1,3 +0,0 @@
-#from mpi_method_server import MpiMethodServer
-
-#__all__ = ['MpiMethodServer']
--- method_server/methods/__init__.py
+++
@@ -1,5 +0,0 @@
-from .mpi_methods import simulate, output_result, NWChem_app
-
-methods_list = ['NWChem_app',
-                'output_result',
-                'simulate']
