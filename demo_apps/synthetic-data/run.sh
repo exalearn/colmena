485dd82a36b3e487193cd2e67f7bb0ca3b26da8b
Greg Pauloski <18683347+gpauloski@users.noreply.github.com>
2022-06-06 17:16:35 +0000

    Improve synthetic app
    - Add support for funcx and parsl task server.
    - Better argument parsing.
    - Support specifying different ProxyStore backends.
    - Remove Jupyter Notebook (will be moved to a different repo where
      we store results/processing scripts).
    - Removed config file support.

--- demo_apps/synthetic-data/run.sh
+++ demo_apps/synthetic-data/run.sh
@@ -5,8 +5,6 @@
 #COBALT -q debug-flat-quad
 #COBALT --attrs enable_shh=0
 
-CONFIG='config.json'
-
 module load miniconda-3/latest
 conda activate colmena
 
@@ -18,17 +16,18 @@ REDIS=$!
 echo "Redis started on $HOSTNAME:$PORT"
 
 python synthetic.py \
-	--redis-host $HOSTNAME \
-	--redis-port $PORT \
-	--task-input-size 50 \
-	--task-output-size 0 \
-	--task-interval 15 \
-	--task-count 50 \
-    --output-dir runs/full_test_unique_30s_50x50_v3 \
-	#--use-value-server \
-    #--reuse-data \
-	#--config $CONFIG
-    #--output-dir runs/full_test_unique_30s_50x50 \
+    --funcx \
+    --endpoint $ENDPOINT_UUID
+    --redis-host $HOSTNAME \
+    --redis-port $PORT \
+    --input-size 1 \
+    --output-size 0 \
+    --interval 1 \
+    --count 20 \
+    --sleep-time 0 \
+    --output-dir runs/funcx_1mb_inputs \
+    --ps-redis
+    --ps-threshold 0.01 \
 
 # Kill the redis server
 kill $REDIS
