c67468a8c701918167c934ae735c2a81dfe7af0a
Logan Ward <WardLT@users.noreply.github.com>
2025-05-02 15:01:59 -0500

    Auto-evict automatic proxies on read (#148)
    * Test current behavior
    
    * Ensure we deregister stores after tests
    
    * Flip behavior for evicting autoproxies

--- colmena/models/results.py
+++ colmena/models/results.py
@@ -252,7 +252,7 @@ class Result(BaseModel):
 
     @classmethod
     def from_args_and_kwargs(cls, fn_args: Sequence[Any], fn_kwargs: Dict[str, Any] = None, **kwargs):
-        """Create a result object form a the arguments and kwargs for the function
+        """Create a result object from the arguments and kwargs for the function
 
         Keyword arguments to this function are passed to the initializer for `Result`.
 
@@ -371,7 +371,7 @@ class Result(BaseModel):
         else:
             store = None
 
-        def _serialize_and_proxy(value, evict=False) -> Tuple[str, int]:
+        def _serialize_and_proxy(value, evict=True) -> Tuple[str, int]:
             """Helper function for serializing and proxying
 
             Args:
--- colmena/tests/test_model.py
+++ colmena/tests/test_model.py
@@ -1,6 +1,9 @@
 """Tests for the data models"""
 import sys
 
+from proxystore.proxy import Proxy
+from proxystore.store.utils import get_key
+
 from colmena.models import ResourceRequirements, Result
 
 
@@ -28,3 +31,21 @@ def test_message_sizes():
     result.serialize()
     assert result.message_sizes['inputs'] >= 2 * sys.getsizeof('0' * 8)
     assert result.message_sizes['inputs'] >= sys.getsizeof(1)
+
+
+def test_proxy(store):
+    result = Result.from_args_and_kwargs(
+        ('a' * 1000,),
+        serialization_method='pickle',
+        proxystore_name='store',
+        proxystore_threshold=10,
+        proxystore_config=store.config()
+    )
+    result.serialize()
+    result.deserialize()
+    assert isinstance(result.inputs[0][0], Proxy)
+    proxy = result.inputs[0][0]
+    key = get_key(proxy)
+
+    assert len(result.args[0]) == 1000
+    assert not store.exists(key)
